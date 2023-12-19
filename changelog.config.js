7785b8ea565f51f01fa7971c711a784a3d84ccd5
ruslan <ager102@gmail.com>
2023-12-19 15:23:48 +0700

    fix: config eslint

--- changelog.config.js
+++ changelog.config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  disableEmoji: false,
+  disableEmoji: true,
   list: ['test', 'feat', 'fix', 'docs', 'refactor', 'style', 'release'],
   maxMessageLength: 80,
   minMessageLength: 3,
@@ -8,37 +8,30 @@ module.exports = {
   types: {
     docs: {
       description: 'Documentation changes',
-      emoji: '',
       value: 'docs',
     },
     feat: {
       description: 'Create new feature / enhance changes',
-      emoji: '',
       value: 'enc',
     },
     fix: {
       description: 'A fixing bugs changes',
-      emoji: '',
       value: 'fix',
     },
     refactor: {
       description: 'Code fixes a minor bug or adds a feature also improve code',
-      emoji: '',
       value: 'refactor',
     },
     release: {
       description: 'Create release commit',
-      emoji: '',
       value: 'release',
     },
     style: {
       description: 'Create / edit style, white-space, formatting,',
-      emoji: '',
       value: 'style',
     },
     test: {
       description: 'Create test & coverage',
-      emoji: '',
       value: 'test',
     },
   },
